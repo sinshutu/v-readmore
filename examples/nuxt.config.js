c78e069fbe5b9bad0990cd6a2ee813eb02f77cb1
NaotoSuzuki <suzuki.naoto@teamup.jp>
2018-07-26 05:59:47 +0900

    run examples

---
+++ examples/nuxt.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'examples',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Vue Read More Example' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  plugins: [
+    {src: '~/plugins/v-readmore', ssr: false}
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
---
+++ examples/plugins/v-readmore.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VReadmore from '../../dist/v-readmore.esm.js'
+
+Vue.use(VReadmore)
