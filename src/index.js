e0915ff0976a02b09ee63e9aa6798270d43b5390
NaotoSuzuki <suzuki.naoto@teamup.jp>
2018-07-26 05:59:15 +0900

    can Vue.use

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
+// vue コンポーネントのインポート
 import VReadmore from './v-readmore.vue'
 
-export default {
-  install: function (Vue) {
-    Vue.component('v-readmore', VReadmore);
-  }
+// Vue.use() によって実行される install 関数を定義
+function install(Vue) {
+	if (install.installed) return;
+	install.installed = true;
+	Vue.component('VReadmore', VReadmore);
 }
+
+// Vue.use() のためのモジュール定義を作成
+// Create module definition for Vue.use()
+const plugin = {
+	install,
+};
+
+// vue が見つかった場合に自動インストールする (ブラウザで <script> タグを用いた場合等)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+	GlobalVue.use(plugin);
+}
+
+// (npm/webpack 等で) モジュールとして利用させるためコンポーネントを export する
+export default plugin;
+export { VReadmore }
